6447b2b57af52415d450e6e4bcbf7cf6a4af7913
Darius Powell <dariusp686@gmail.com>
2011-03-21 20:28:14 +1100

    Added regular find cmd.  Made all cmds position point in the quotes.

--- grin.el
+++ grin.el
@@ -2,6 +2,8 @@
 
 (defvar grind-hist nil)
 
+(defvar grind-find-hist nil)
+
 (defgroup grin nil
   "Run grin and grind (python replacements for grep and find) putting hits in a grep buffer."
   :group 'tools
@@ -19,13 +21,26 @@
 
 (defun grin ()
   (interactive)
-  (let* ((cmd (read-shell-command "Command: " (concat grin-cmd " ") 'grin-hist))
+  (let* ((c (concat grin-cmd " \"\""))
+         (l (length c))
+         (cmd (read-shell-command "Command: " (cons c l) 'grin-hist))
          (null-device nil))
     (grep cmd)))
 
 (defun grind ()
   (interactive)
-  (let* ((args (read-shell-command "Command: " (concat grind-cmd " ") 'grind-hist))
+  (let* ((c (concat grind-cmd " \"\""))
+         (l (length c))
+         (args (read-shell-command "Command: " (cons c l) 'grind-hist))
+         (cmd (concat args " | sed s/$/\\:1\\:/"))
+         (null-device nil))
+    (grep cmd)))
+
+(defun grind-find ()
+  (interactive)
+  (let* ((c "find . -name \"CVS\" -prune -o -name \"RCS\" -prune -o -name \".svn\" -prune -o -name \".hg\" -prune -o -name \".bzr\" -prune -o -name \".git\" -o -name \"build\" -prune -o -name \"dist\" -prune -o -name \"*.pyc\" -prune -o -name \"*.pyo\" -prune -o -name \"*.so\" -prune -o -name \"*.o\" -prune -o -name \"*.a\" -prune -o -name \"*.tgz\" -prune -o -name \"*.tar.gz\" -prune -o -name \"*.rar\" -prune -o -name \"*.zip\" -prune -o -name \"*~\" -prune -o -name \"#\" -prune -o -name \"*.bak\" -prune -o -name \"*.png\" -prune -o -name \"*.jpg\" -prune -o -name \"*.gif\" -prune -o -name \"*.bmp\" -prune -o -name \"*.tif\" -prune -o -name \"*.tiff\" -prune -o -name \"*.pyd\" -prune -o -name \"*.dll\" -prune -o -name \"*.exe\" -prune -o -name \"*.obj\" -prune -o -name \"*.lib\" -prune -o -iname \"\" -print")
+         (l (- (length c) 7))
+         (args (read-shell-command "Command: " (cons c l) 'grind-find-hist))
          (cmd (concat args " | sed s/$/\\:1\\:/"))
          (null-device nil))
     (grep cmd)))
